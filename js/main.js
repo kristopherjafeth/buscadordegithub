c2647183161686304d702534abd264b069787032
kristopherjafeth <kristopherjafeth@gmail.com>
2023-02-13 09:48:37 -0400

    V1.0

---
+++ js/main.js
@@ -0,0 +1,54 @@
+const API = "https://api.github.com/users/";
+
+async function doSearch() {
+  let response = await fetch(API + "kristopherjafeth");
+  const data = await response.json();
+  console.log(data);
+}
+
+const app = Vue.createApp({
+  data() {
+    return {
+      search: null,
+      result: null,
+      error: null,
+      favorites: new Map()
+    };
+  },
+
+  computed:{
+    isFavorite(){
+        return this.favorites.has(this.result.id) 
+    },
+    allFavorites(){
+       return Array.from(this.favorites.values())
+    }
+
+  },
+
+  methods: {
+    async doSearch() {
+        this.result = this.error = null
+      try {
+        const response = await fetch(API + this.search);
+        if(!response.ok) throw new Error("Usuario no encontrado")
+        const data = await response.json();
+        console.log(data);
+        this.result = data;
+      } catch (error) {
+        this.error = error
+      } finally {
+        this.search = null
+      }
+    },
+
+    addFavorite() {
+        this.favorites.set(this.result.id, this.result)
+    },
+
+    removeFavorite() {
+        this.favorites.delete(this.result.id)
+    }
+
+  },
+});
